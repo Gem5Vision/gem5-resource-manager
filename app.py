f15412220de014cc8bda2a6647eba017c06674dc
Parth Shah <helloparthshah@gmail.com>
2023-02-10 19:43:46 -0800

    viewing the json file using pandas

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import json
+import pandas as pd
 
 with open('resources_new.json') as f:
     data = json.load(f)
     resources = data['resources']
-    for resource in resources:
-        print(resource['type'])
+    df = pd.DataFrame(resources)
+    # get all categories
+    categories = df['type'].unique()
+    print(categories)
+    # get names of all the groups
+    groups = df[df['group'].notnull()]['group'].unique()
+    print(groups)
+    # get all types of architectures
+    archs = df[df['architecture'].notnull()]['architecture'].unique()
+    print(archs)
