e25c7bc878261f9bac94696f8a5cd13958e1a55a
Parth Shah <helloparthshah@gmail.com>
2023-02-12 21:05:13 -0800

    fetching the file from source

--- create_new_json.py
+++ create_new_json.py
@@ -1,4 +1,7 @@
 import json
+import requests
+import base64
+import pandas as pd
 
 
 def change_type(resource):
@@ -21,20 +24,71 @@ def change_type(resource):
     return resource
 
 
-# create new json file
-with open('resources_new.json', 'w') as newf:
-    with open('resources.json') as f:
-        data = json.load(f)
-        new_resources = []
-        resources = data['resources']
-        for resource in resources:
-            if resource['type'] == 'group':
-                for group in resource['contents']:
-                    group['group'] = resource['name']
-                    group = change_type(group)
-                    new_resources.append(group)
-            else:
-                resource = change_type(resource)
-                new_resources.append(resource)
-        data['resources'] = new_resources
-        newf.write(json.dumps(data))
+# fetch file from google source at https://gem5.googlesource.com/public/gem5-resources/+/refs/heads/stable/resources.json
+# and save it as resources.json
+data = requests.get(
+    'https://gem5.googlesource.com/public/gem5-resources/+/refs/heads/stable/resources.json?format=TEXT').text
+data = base64.b64decode(data).decode('utf-8')
+
+with open('resources.json', 'w') as newf:
+    data = json.loads(data)
+    new_resources = []
+    resources = data['resources']
+    for resource in resources:
+        if resource['type'] == 'group':
+            for group in resource['contents']:
+                group['group'] = resource['name']
+                group = change_type(group)
+                new_resources.append(group)
+        else:
+            resource = change_type(resource)
+            new_resources.append(resource)
+    
+    df = pd.DataFrame(new_resources)
+    # get all categories
+    categories = df['type'].unique()
+    print(categories)
+    # get names of all the groups
+    groups = df[df['group'].notnull()]['group'].unique()
+    print(groups)
+    # get all types of architectures
+    archs = df[df['architecture'].notnull()]['architecture'].unique()
+    print(archs)
+
+    print(df[df['type'] == 'resource']['name'])
+    # print(df[df['architecture'] == 'ARM']['name'])
+
+    # rename name column to id
+    df.rename(columns={'name': 'id'}, inplace=True)
+
+    # rename type column to category
+    df.rename(columns={'type': 'category'}, inplace=True)
+
+    # rename url column to download_url
+    df.rename(columns={'url': 'download_url'}, inplace=True)
+
+    # rename documentation column to description
+    df.rename(columns={'documentation': 'description'}, inplace=True)
+
+    # add new column for name and initialize it with id but replace all - with space
+    df['name'] = df['id'].str.replace('-', ' ')
+
+    # add new column for gem5_version and initialize it with 22.1
+    df['gem5_version'] = '22.1'
+
+    # add downloads column and initialize it to 0
+    df['downloads'] = 0
+
+    # add example_url column and initialize it to None
+    df['example_url'] = None
+
+    # add documentation_url column and initialize it to None
+    df['documentation_url'] = None
+
+    # add github_url column and initialize it to None
+    df['github_url'] = None
+    # print to json and replace Nan with None
+    df = df.where((pd.notnull(df)), None)
+    resources = df.to_dict('records')
+    data['resources'] = resources
+    newf.write(json.dumps(data, indent=4))
