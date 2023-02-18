160ff7c40431eafc2cf18809025ba253e9eb9ef3
Parth Shah <helloparthshah@gmail.com>
2023-02-18 00:21:28 -0800

    updated github url

--- create_new_json.py
+++ create_new_json.py
@@ -30,8 +30,9 @@ def change_type(resource):
 # fetch file from google source at https://gem5.googlesource.com/public/gem5-resources/+/refs/heads/stable/resources.json
 # and save it as resources.json
 data = requests.get(
-    'https://gem5.googlesource.com/public/gem5-resources/+/refs/heads/stable/resources.json?format=TEXT').text
+    'https://gem5.googlesource.com/public/gem5-resources/+/refs/heads/develop/resources.json?format=TEXT').text
 data = base64.b64decode(data).decode('utf-8')
+
 
 with open('resources.json', 'w') as newf:
     data = json.loads(data)
@@ -78,15 +79,17 @@ with open('resources.json', 'w') as newf:
 
     # add new column for gem5_version and initialize it with 22.1
     df['gem5_version'] = '22.1'
-
+    # add author column and initialize it to empty list
+    df['author'] = None
+    df['tags'] = None
     # add downloads column and initialize it to 0
-    df['downloads'] = 0
+    # df['downloads'] = 0
 
     # add example_url column and initialize it to None
-    df['example_url'] = None
+    # df['example_url'] = None
 
     # add documentation_url column and initialize it to None
-    df['documentation_url'] = None
+    # df['documentation_url'] = None
 
     # add github_url column and initialize it to None
     df['github_url'] = None
@@ -95,3 +98,42 @@ with open('resources.json', 'w') as newf:
     resources = df.to_dict('records')
     data['resources'] = resources
     newf.write(json.dumps(data, indent=4))
+
+
+with open('resources.json', 'r+') as f:
+    data = json.load(f)
+    for resource in data['resources']:
+        if resource['source'] is not None:
+            try:
+                print(resource['source'])
+                resource['github_url'] = 'https://github.com/gem5/gem5-resources/tree/develop/' + \
+                    str(resource['source'])
+                request = requests.get(
+                    'https://raw.githubusercontent.com/gem5/gem5-resources/develop/'+str(resource['source'])+'/README.md')
+                content = request.text
+                # get contnt between --- and ---
+                content = content.split('---')[1]
+                content = content.split('---')[0]
+                tags = content.split('tags:\n')[1]
+                # get all tags till there is no indentation
+                tags = tags.split('\n')
+                for i in range(len(tags)):
+                    if not tags[i].startswith(' '):
+                        tags = tags[:i]
+                        break
+                # trim white spaces
+                tags = [tag.strip().replace('- ', '') for tag in tags]
+                resource['tags'] = tags
+                # get author
+                author = content.split('author:')[1]
+                author = author.split('\n')[0]
+                # covert ["Name1", "Name2"] to a list of strings
+                author = author.replace(
+                    '[', '').replace(']', '').replace('"', '')
+                author = author.split(',')
+                author = [a.strip() for a in author]
+                resource['author'] = author
+            except:
+                pass
+    f.seek(0)
+    f.write(json.dumps(data, indent=4))
