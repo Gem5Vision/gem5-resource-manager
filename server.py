08e3c5b268dc00b6b65df3dcf4cd16bdde3b2b57
aarsli <arslanali9169@gmail.com>
2023-04-19 23:44:59 -0700

    404 page

--- server.py
+++ server.py
@@ -93,5 +93,10 @@ def insert():
     return {"status": "Inserted"}
 
 
+@app.errorhandler(404)
+def handle404(error):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(debug=True)
