9b3199051dc10d687f80c761ef7c136624a1987e
aarsli <arslanali9169@gmail.com>
2023-04-24 15:59:40 -0700

    Update to Alerts Styling

--- static/app.js
+++ static/app.js
@@ -1,28 +1,35 @@
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
 
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
+const appendAlert = (errorHeader, message, type) => {
+  const wrapper = document.createElement('div');
   wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible fade show d-flex align-items-center" role="alert">`,
-    `   <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-3" height="1.5rem" viewBox="0 0 16 16" role="img" aria-label="Warning:">`,
-    `     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>`,
-    `   </svg>`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+    `<div class="alert alert-${type} alert-dismissible fade show d-flex flex-column shadow-sm" role="alert">`,
+    `  <div class="d-flex align-items-center main-text-semi">`,
+    `    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1.5rem" class="bi bi-exclamation-octagon-fill me-3" viewBox="0 0 16 16">`,
+    `      <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 
+              .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM8 4c.535 0 .954.462.9.995l-.35 
+              3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>`,
+    `    </svg>`,
+    `    <span class="main-text-regular">${errorHeader}</span>`,
+    `      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`,
+    `    </div>`,
+    `  <hr />`,
+    `  <div>${message}</div>`,
+    `</div>`
+  ].join('');
 
-  alertPlaceholder.append(wrapper)
+  alertPlaceholder.append(wrapper);
+
+  setTimeout(function() {
+    bootstrap.Alert.getOrCreateInstance(document.querySelector(".alert")).close();
+  }, 3000);
 }
 
 function loadPrevSession(event) {
   event.preventDefault();
   const prevSession = localStorage.getItem("URI");
   if (!prevSession) {
-    appendAlert('ERROR! No Saved Session!', 'danger');
-    setTimeout(function() {
-      bootstrap.Alert.getOrCreateInstance(document.querySelector(".alert")).close();
-    }, 3000);
+    appendAlert('Error!', 'No Saved Session!', 'danger');
   } else {
     const url = '/editor?uri=' + encodeURIComponent(prevSession); 
     window.location = url;
@@ -37,6 +44,11 @@ function handleLogin(event, saveStatus) {
     localStorage.setItem("URI", uri);
   }
 
+  if (uri === "") {
+    appendAlert('Error!', 'Cannot Proceed With Empty URI!', 'danger');
+    return;
+  }
+
   const url = '/validateURI?uri=' + encodeURIComponent(uri);
   fetch(url,
   {
@@ -46,12 +58,10 @@ function handleLogin(event, saveStatus) {
       }
   })
   .then((res) => {
-    console.log("URI Validation Response Status: " + res.status)
+    console.log("URI Validation Response Status: " + res.status);
+    
     if (res.status === 400) {
-      appendAlert('ERROR! Cannot Proceed With Empty URI!', 'danger');
-      setTimeout(function() {
-        bootstrap.Alert.getOrCreateInstance(document.querySelector(".alert")).close();
-      }, 3000);
+      appendAlert('Error!', 'Cannot Proceed With Empty URI!', 'danger');
     }
     
     if (res.redirected) {
